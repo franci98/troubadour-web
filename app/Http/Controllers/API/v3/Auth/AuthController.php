dc6c0de33a6a00ac83c72fe51d95d795e2fb97ee
Franci <franciklavz@gmail.com>
2021-12-14 18:00:42 +0100

    Registraton WiP

--- app/Http/Controllers/API/v3/Auth/AuthController.php
+++ app/Http/Controllers/API/v3/Auth/AuthController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|unique:users,name',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed'
+        ]);
+    }
 }
