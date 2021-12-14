3025303fed2425bc1bbb9853275d039cb2adfc0f
Franci <franciklavz@gmail.com>
2021-12-14 17:53:57 +0100

    Prepared swagger

---
+++ app/Http/Controllers/API/v3/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Http\Controllers\API\v3;
+
+
+class Controller
+{
+    /**
+     * @OA\Info(
+     *     version="3.0.0",
+     *     title="Troubadour OpenApi Documentation",
+     *     description="Documentation to help with the API for Troubadour platform. The platform offers automatic generation of exercises, which can be further adjusted by individual teachers. It is a web-based platform with a responsive interface that adjusts its layout to different mobile devices. Based on the individual students' exam results gathered in this research, the exercises can be further adjusted specifically to improve the individual student's performance. Focused on the music theory learning, the Troubadour platform offers an intuitive visual representation of the music score. With gamification features, including badges, leaderboards and a multiplayer mode---the Troubadour platform aims to engage the students in exercising and boosting their knowledge. The platform therefore represents a student-engaging learning environment for music theory, supplementing existing learning management systems, such as Moodle.",
+     *     @OA\Contact(
+     *         email="franciklavz@gmail.com"
+     *     ),
+     *     @OA\License(
+     *         name="Apache 2.0",
+     *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *     )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Local"
+     * )
+     *
+     * @OA\Tag(
+     *     name="Users",
+     *     description="API endpoints for user"
+     * )
+     */
+
+}
