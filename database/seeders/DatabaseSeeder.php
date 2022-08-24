a96fbeabe098480b1deea3ef8fbdb1f5bb4e9d4d
Franci <franciklavz@gmail.com>
2022-08-24 15:17:04 +0200

    User admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(SchoolSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(GameTypeSeeder::class);
         $this->call(DifficultySeeder::class);
         $this->call(RyhthmExerciseConfigSeeder::class);
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\RoleUser;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::query()->firstOrCreate([
+            'name' => 'Trubadur admin',
+            'email' => 'info@trubadur.si',
+            'school_id' => 1
+        ], [
+            'email_verified_at' => now(),
+            'password' => Hash::make('test1234'),
+        ]);
+        $user->roles()->sync([Role::ADMIN => ['status' => RoleUser::STATUS_ACTIVE], Role::TEACHER => ['status' => RoleUser::STATUS_ACTIVE]]);
+    }
+}
