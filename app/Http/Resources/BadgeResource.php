164ecad2b75a07dd6c428a2e1cc37f4d271ff0e3
Franci Klavž <franciklavz@gmail.com>
2022-11-19 18:14:33 +0100

    Badge fix

--- app/Http/Resources/BadgeResource.php
+++ app/Http/Resources/BadgeResource.php
@@ -13,15 +13,15 @@ class BadgeResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-
     {
+        $isAchieved = $this->resource->hasBeenAchievedBy(auth()->user());
         $data = [
             'id' => $this->resource->id,
             'title' => $this->resource->title,
             'description' => $this->resource->description,
             'image_url' => url('/img/badges/' . $this->resource->id . '.svg'),
-            'progress' => 0,
-            'is_achieved' => $this->resource->hasBeenAchievedBy(auth()->user()),
+            'progress' => $isAchieved ? 1 : 0,
+            'is_achieved' => $isAchieved
         ];
         if ($data['is_achieved']) {
             $data['achieved_at'] = auth()->user()->badges()->where('badge_id', $this->resource->id)->first()->pivot->created_at;
